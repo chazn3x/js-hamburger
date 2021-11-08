18fb93a6aa6bd8821e04086b7dfddd85f75896c0
Giovanni <gio.a.98@gmail.com>
2021-11-08 13:20:29 +0100

    test click menu

---
+++ main.js
@@ -0,0 +1,6 @@
+// seleziono il menu
+const openMenu = document.querySelector(".header-right > a");
+// rendo il menu cliccabile
+openMenu.addEventListener("click", function(){
+    alert("ciao");
+});
