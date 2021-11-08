4da57342ec3241c9bbc5beeba613efcca57169c7
Giovanni <gio.a.98@gmail.com>
2021-11-08 13:27:05 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ openMenu.addEventListener("click", function(){
     // seleziono il menu e gli aggiungo la classe active
     document.querySelector(".hamburger-menu").classList.add("active")
 });
+// seleziono la x nel menu
+const closeMenu = document.querySelector(".close");
+// rendo la x cliccabile
+closeMenu.addEventListener("click", function(){
+    // seleziono il menu e gli tolgo la classe active
+    document.querySelector(".hamburger-menu").classList.remove("active")
+});
